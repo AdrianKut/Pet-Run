a3a370467f5dfacfe339e280c0472a86a5eb364d
unknown <adrian.kut96@onet.pl>
2022-09-05 14:44:06 +0200

    Create camera movement

---
+++ Assets/Scripts/Core/CameraManager.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField] private float _forceJump;
+    [SerializeField] private float _startSpeedValue;
+    private float _speed;
+
+    [SerializeField] private float _mouseSensitivityX = 5f;
+    [SerializeField] private float _mouseSensitivityY = 5f;
+    private float rotY = 0.0f;
+
+    void Update()
+    {
+        CameraMovement();
+        Sprint();
+        JumpAndCrouch();
+    }
+
+    private void CameraMovement()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal") * _speed;
+        float moveVertical = Input.GetAxis("Vertical") * _speed;
+
+        transform.Translate(Vector3.forward * moveVertical * Time.deltaTime);
+        transform.Translate(Vector3.right * moveHorizontal * Time.deltaTime);
+
+        if (Input.GetMouseButton(1))
+        {
+            float rotX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * _mouseSensitivityX;
+            rotY += Input.GetAxis("Mouse Y") * _mouseSensitivityY;
+            rotY = Mathf.Clamp(rotY, -90f, 90f);
+            transform.localEulerAngles = new Vector3(-rotY, rotX, 0.0f);
+        }
+    }
+
+    private void JumpAndCrouch()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(Vector3.up * _forceJump * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            transform.Translate(Vector3.down * _forceJump * Time.deltaTime);
+        }
+    }
+
+    private void Sprint()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            _speed = _startSpeedValue * 2;
+        }
+        else
+        {
+            _speed = _startSpeedValue;
+        }
+    }
+}
