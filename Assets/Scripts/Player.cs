c78b33e69e011fc148c19d290ede5e88cdc9250d
unknown <adrian.kut96@onet.pl>
2022-09-08 21:18:06 +0200

    Create Player Script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.AI;
+using TMPro;
+using static UnityEngine.GraphicsBuffer;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Transform _destinationTransformPosition;
+    [SerializeField] private TextMeshPro _playerNickname;
+    [SerializeField] private float _speed;
+
+    public NavMeshAgent Agent { get; private set; }
+
+    private void Awake()
+    {
+        Agent = this.gameObject.GetComponent<NavMeshAgent>();
+        Agent.isStopped = true;
+    }
+
+    private void LateUpdate()
+    {
+        RotateNicknameToCamera();
+       // CalculateDistanceToDistancePosition();
+    }
+
+    public void SetDestinationPosition(Transform target)
+    {
+        _destinationTransformPosition = target;
+        Agent.SetDestination(_destinationTransformPosition.position);
+    }
+
+    public float GetDistanceToDestiantionPosition()
+    {
+        return (transform.position - _destinationTransformPosition.transform.position).sqrMagnitude;
+    }
+
+    private void RotateNicknameToCamera()
+    {
+        _playerNickname.transform.rotation = Camera.main.transform.rotation;
+    }
+}
