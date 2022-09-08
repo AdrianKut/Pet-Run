ddfa58ba9e5a5e3b84a1e95fc9a9e041dc50dd4d
unknown <adrian.kut96@onet.pl>
2022-09-08 21:19:43 +0200

    Create Popup Text Script

---
+++ Assets/Scripts/TextOnScreen.cs
@@ -0,0 +1,21 @@
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+
+public class TextOnScreen : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _textMesh;
+    [SerializeField] private Vector3 _endScaleValue;
+    [SerializeField] private float _scaleDuration;
+
+    void OnEnable()
+    {
+        _textMesh.gameObject.transform.DOScale(_endScaleValue, _scaleDuration);
+        Destroy(this.gameObject, 1f);
+    }
+
+    public void SetText(string text)
+    {
+        _textMesh.text = text;
+    }
+}
