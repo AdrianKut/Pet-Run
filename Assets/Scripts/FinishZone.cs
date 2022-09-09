bcd5f38334a1ef9cd92548dae89f16892283d3e0
unknown <adrian.kut96@onet.pl>
2022-09-09 19:00:56 +0200

    Update FinishZone Script

--- Assets/Scripts/FinishZone.cs
+++ Assets/Scripts/FinishZone.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class FinishZone : MonoBehaviour
 {
-    [SerializeField] private GameObject _gameObjectStarPrefab;
+    [SerializeField] private GameObject _gameObjectFinishRaceVFX;
 
     private void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.GetComponent<Player>())
         {
             GameManager.Instance.Winner(collider.gameObject);
-            var gameObject = Instantiate(_gameObjectStarPrefab, this.gameObject.transform.position, Quaternion.identity);
+            var gameObject = Instantiate(_gameObjectFinishRaceVFX, this.gameObject.transform.position, Quaternion.identity);
             Destroy(gameObject, 1f);
         }
     }
