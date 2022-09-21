27996c3dac27092cec6eaeff3814d59b75d51033
unknown <adrian.kut96@onet.pl>
2022-09-21 13:18:57 +0200

    Create level design scene

--- Assets/Prefabs/Obstacles/TrapsManager.cs
+++ Assets/Prefabs/Obstacles/TrapsManager.cs
@@ -10,8 +10,11 @@ public class TrapsManager : MonoBehaviour
     [SerializeField] private float _force = 2500f;
 
     [Header("Spike")]
-    [SerializeField] private GameObject _glassGameObject;
+    [SerializeField] private GameObject _gameObjectGlass;
     [SerializeField] private float _durationMoveX = 1f;
+
+    [Header("Balls")]
+    [SerializeField] private GameObject _gameObjectBalls;
 
     void Update()
     {
@@ -41,9 +44,9 @@ public class TrapsManager : MonoBehaviour
 
     private IEnumerator MoveSpikeGlass()
     {
-        var startXPos = _glassGameObject.transform.position.x;
-        _glassGameObject.transform.DOMoveX(startXPos - 9.4f, _durationMoveX);
+        var startXPos = _gameObjectGlass.transform.position.x;
+        _gameObjectGlass.transform.DOMoveX(startXPos - 9.4f, _durationMoveX);
         yield return new WaitForSeconds(5f);
-        _glassGameObject.transform.DOMoveX(startXPos, _durationMoveX);
+        _gameObjectGlass.transform.DOMoveX(startXPos, _durationMoveX);
     }
 }
